bcce32bbe2993e17002e46bc41a20b2904107207
Vinícius Simões <vinicius.simoes95@gmail.com>
2019-04-08 21:15:53 -0300

    add more tests and doc tests

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -19,6 +19,16 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+  Determines whether a deck contains a given card
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
@@ -31,7 +41,7 @@ defmodule Cards do
   ## Examples
 
       iex> deck = Cards.create_deck
-      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> {hand, _deck} = Cards.deal(deck, 1)
       iex> hand
       ["Ace of Spades"]
 
